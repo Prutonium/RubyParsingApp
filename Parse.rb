f8f9f816b8e2e3484f8527baca3f9adeeff966b2
Brandon Pruett <BrandonPruett819@gmail.com>
2016-10-10 22:26:21 -0500

    Update Parse.rb
    attempting to split line string from file into variables

--- Parse.rb
+++ Parse.rb
@@ -1,4 +1,11 @@
 file='http_access_log'
 File.readlines(file).each do |line|
-  puts line
+  var1 = line.split[0]
+  var2 = line.split[3]
+  var3 = line.split[6]
+  var4 = line.split[8]
+  puts var1
+  puts var2
+  puts var3
+  puts var4
 end
