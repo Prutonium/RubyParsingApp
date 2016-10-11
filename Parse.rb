4475bda974b952e5d069832e3b45b8ed26d31686
Brandon Pruett <BrandonPruett819@gmail.com>
2016-10-10 22:12:21 -0500

    Update Parse.rb
    starting smaller

--- Parse.rb
+++ Parse.rb
@@ -1,6 +1,4 @@
-lines.each do |line|
-  matches = /^(\S+).*GET\s(.*)\sHTTP\S*\s(\d+)/.match(line)
-  time = matches[1]
-  request = matches[2]
-  status = matches[3]
-do
+file='http_access_log'
+File.readlins(file).each do |line|
+  puts line
+end
