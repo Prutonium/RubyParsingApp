ea5b29cfa4d59e08bf28cbe6ef0ffe678df7d7e9
Brandon Pruett <BrandonPruett819@gmail.com>
2016-10-12 00:08:11 -0500

    Update Parse.rb

--- Parse.rb
+++ Parse.rb
@@ -1,11 +1,11 @@
 file='http_access_log'
 File.readlines(file).each do |line|
-  date = line.split[0]
+  var1 = line.split[0]
   date = var1[1..11]
   var2 = line.split[3]
   var3 = line.split[6]
   var4 = line.split[8]
-  puts var1
+  puts date
   puts var2
   puts var3
   puts var4
