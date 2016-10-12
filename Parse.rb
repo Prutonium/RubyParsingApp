040fc622d0a3def29585755477219b70f5e92c26
Brandon Pruett <BrandonPruett819@gmail.com>
2016-10-12 00:11:27 -0500

    Update Parse.rb
    correcting which variable date comes from

--- Parse.rb
+++ Parse.rb
@@ -1,12 +1,12 @@
 file='http_access_log'
 File.readlines(file).each do |line|
   var1 = line.split[0]
-  date = var1[1..11]
   var2 = line.split[3]
+  date = var2[1,11]
   var3 = line.split[6]
   var4 = line.split[8]
+  puts var1
   puts date
-  puts var2
   puts var3
   puts var4
 end
