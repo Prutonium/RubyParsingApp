706b8c08cd013c380cf502cbf2a89a3eb5028170
Brandon Pruett <BrandonPruett819@gmail.com>
2016-10-12 00:44:08 -0500

    Update Parse.rb
    commenting thing out. having issue iterating past first date, as the app crashes after the Date variable actually changes to Oct 25

--- Parse.rb
+++ Parse.rb
@@ -1,12 +1,19 @@
+#date_array = []
+date = ''
 file='http_access_log'
 File.readlines(file).each do |line|
   var1 = line.split[0]
   var2 = line.split[3]
   date = var2[1,11]
+  #date_array.push(date)
   var3 = line.split[6]
   var4 = line.split[8]
-  puts var1
+  #puts var1
+  #puts var2
   puts date
-  puts var3
-  puts var4
+  #puts var3
+  #puts var4
+  #puts date_array
 end
+
+#while
