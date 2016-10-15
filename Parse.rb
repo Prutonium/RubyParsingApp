ac3684f77320e91e5b53e4a3d311ede119081a49
Brandon Pruett <BrandonPruett819@gmail.com>
2016-10-15 10:37:19 -0500

    Update Parse.rb
    added arrray for dates

--- Parse.rb
+++ Parse.rb
@@ -1,25 +1,28 @@
-#date_array 
+date_array = []
 file='http_access_log_cleanedup.txt'
 error = 0
 ln = 1
 
 File.readlines(file).each do |line|
   ln += 1
-  matches = /.* \[(.*) \-\d{4}\] "(.*?)"\W(\d{3})/	.match(line)
+  matches = /.* \[(.*) \-\d{4}\] "GET (.*?) HTTP\/1.0"\W(\d{3})/	.match(line)
   if matches == nil
 	puts "Error line"
 	error = error + 1
   else
 	date = matches[1]
+	day = date[0..10]
+	date_array.push(day)
 	download = matches[2]
 	outcome =  matches[3]
-	puts date
+	#puts date
+	puts day
 	puts download
 	puts outcome
   end
   #date_array.push(date)
 
-  puts ln
+puts ln
 
 
   
